d33177c815e588a4380213aa2951f221fb0d3952
lucylinGIT <llin0821@hotmail.com>
2020-12-07 17:06:55 -0500

    fdfd

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,10 +1,12 @@
 // Main javascript: Lucy Lin
+// inspiration: https://www.w3schools.com/jsref/event_onclick.asp /
 function erikcontactbutton() {
     document.getElementById("erikemail").innerHTML = "erik@elconsulting.ca";
 }
 function lucycontactbutton() {
     document.getElementById("lucyemail").innerHTML = "lucy@elconsulting.ca";
 }
+// inspiration: https://www.w3schools.com/jquery/jquery_fade.asp 
 $(document).ready(function(){
     $("#button1").click(function(){
         $("#phonenumber").fadeIn(1500);
